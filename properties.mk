54b20ed2dcecfad192ea8c60d131c570643babd2
Praveen Chavan <pchavan@codeaurora.org>
2023-11-13 13:34:25 +0530

    sweet_k6a: Add a property to enable prefetching video
    persist.mm.enable.prefetch, if set to true, will enable
    prefetching of video input from upstream source to help
    improve playback performance for high frame-rate and
    high bitrate content
    
    Change-Id: I814785befd9844364afb50eb7d48439406ae0750
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- properties.mk
+++ properties.mk
@@ -282,7 +282,8 @@ ro.lmk.use_minfree_levels=true
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
 debug.stagefright.omx_default_rank=0 \
-debug.stagefright.ccodec=1
+debug.stagefright.ccodec=1 \
+persist.mm.enable.prefetch=true
 
 PRODUCT_SYSTEM_PROPERTIES += \
 media.aac_51_output_enabled=true \
